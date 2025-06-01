84a94c89c256d309249a9e77a8c2be3e0e959734
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-01 16:47:05 -0300

    feat: implement Doctors page with layout and components for managing doctors

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -9,10 +9,9 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -60,6 +59,7 @@ const items = [
 export function AppSidebar() {
   const router = useRouter();
   const session = authClient.useSession();
+  const pathname = usePathname();
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -83,7 +83,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,37 @@
+import { Plus } from "lucide-react";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+const Doctors = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default Doctors;
