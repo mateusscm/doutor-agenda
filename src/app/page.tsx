af9d90906351f3d03c775e9871d06d7b617f3597
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-06 16:32:43 -0300

    feat: simplify home page by redirecting to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <Button>AAAAAA</Button>
-    </div>
-  );
+  return redirect("/dashboard");
 }
